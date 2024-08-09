814804a057ed30202a6c4d8547469d958bc9fa3d
counterat <counteratios@gmail.com>
2024-08-09 11:04:36 +0300

    s

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 const AdController = (window as any).Adsgram.init({ blockId: "1648" });
 
 export {AdController}
-export const socket = new WebSocket('ws://testapi.tappybrd.com/ws');
+export const socket = new WebSocket('wss://testapi.tappybrd.com/ws');
 root.render(
 	
 		<App />
